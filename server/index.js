09ef388987c95c2addf168035d33ac27e9f0d3da
Mohit <mohitsihare301@gmail.com>
2023-12-28 13:36:46 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -14,10 +14,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use('/user',userRoutes);
-
+const CONNECTION_URL="mongodb+srv://mohit:mern1234@cluster0.m97blhx.mongodb.net/test?retryWrites=true&w=majority"
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(error.message));
 
